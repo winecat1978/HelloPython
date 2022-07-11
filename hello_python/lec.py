be6eeae7739df254ceff6dd6dcce9bc9120504da
Katherine_20 <mob1911494@gmail.com>
2022-07-11 22:04:18 +0300

    added first lecture

---
+++ hello_python/lec.py
@@ -0,0 +1,95 @@
+# print ('hello world')
+# value = None
+# print(type(value))
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+
+# value = 122334
+# print(value)
+# print(type(value))
+
+# s = 'hello world' 
+# t = "bye \nworld" # new line \n
+# print(s)
+# print(t)
+
+# print(a,' - ',b, ' - ',s)
+# print('{2} - {0} - {1}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+
+# print('Write a: ');
+# a = float(input())
+# print('Write b: ');
+# b = float(input())
+# print(a, ' + ',b, ' = ',a+b)
+
+# list = ['1','2','3']
+# col = ['hello',1,2,4,5,True]
+# print(list)
+# print(col)
+
+# a = 1.33451
+# b = 3
+# c = round(a*b, 5) # // деление без остатка, ** - возведение в степень
+# print(c)
+
+# a = 3
+# a+=5
+
+# a = 1 < 4 and 5 > 2
+# print(a)
+
+# a = 1< 3 <5 <7
+# print(a)
+
+# func = 1
+# T = 4
+# x = 2
+# print(func<T>(x))
+
+# f = 1 > 2 or 6 < 3
+
+# f = [1,2,3,4]
+# print(not 2 in f) # checks if an object is in a list
+
+# is_odd = not f[0] % 2
+# print(is_odd)
+
+#a = int(input('a = '))
+#b = int(input('b = '))
+#if a > b:
+#    print(a)
+#else:
+#    print(b)
+
+# if - elif - elif - ... - elif - else:
+
+#original = 46
+#inverted = 0
+#while original != 0:
+    #inverted = inverted * 10 + (original % 10)
+    #original //= 10
+    #print(original)
+#else:
+    #print ('Пожалуй')
+    #print('хватит')
+#print(inverted)
+
+for i in 1,2,3,4,5:
+    print(i**2)
+
+r = range(10)
+for y in r:
+    print(y)
+
+for a in range(1,10, 2):
+    print(a)
+
+for b in 'qwerty':
+    print(b)
